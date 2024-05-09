f030bcea33924ebb065fe526e08178cc2a21adde
Kashif <ahmed.kashif1001@gmail.com>
2024-05-09 23:59:37 +0500

    homepage books fetch using axios

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,8 +1,23 @@
 //We will show all the books here in the homepage
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Books = () => {
+  
+  const [books, setBooks] = useState([])
+
+  useEffect(()=>{
+    const fetchAllBooks = async () =>{
+      try{
+        const res = axios.get("")
+
+      }catch(err){
+
+      }
+    }
+  })
+
   return (
     <div>Books</div>
   )
