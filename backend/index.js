ae720afd5b8ae7968b85b24746cd842d70551194
Kashif <ahmed.kashif1001@gmail.com>
2024-05-09 16:01:42 +0500

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+import express from "express";
+import mysql from "mysql";
+
+const app = express()
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"KA44$kah",
+    database:"test"
+})
+
+
+app.listen(8800, ()=>{
+    console.log("Connected to Backend!");
+})    
